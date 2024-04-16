f5af1252eea0c384f24ab750c94ef42e17547fb1
Nimit kumar <nimitkumar2002@gmail.com>
2024-04-16 16:13:39 +0530

    pong day-1 update

--- main.lua
+++ main.lua
@@ -1,19 +1,31 @@
+push = require 'push'
+
 WINDOW_HIEGHT=720
 WINDOW_WIDTH=1280
 
+VIRTUAL_WIDTH =432
+VIRTUAL_HIEGHT=243
+
 function love.load()
-    love.window.setMode(WINDOW_WIDTH,WINDOW_HIEGHT,{
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HIEGHT, WINDOW_WIDTH, WINDOW_HIEGHT,{
         fullscreen = false,
         resizable = false,
         vsync = true
     })
 end
 
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    end
+end
+
 function love.draw()
-    love.graphics.printf(
-        'Hello Pong!',
-        0,
-        WINDOW_HIEGHT / 2 - 6,
-        WINDOW_WIDTH,'center'
-    )
+    push:apply('start')
+
+    love.graphics.printf('Hello Pong!',0,VIRTUAL_HIEGHT / 2 - 6,VIRTUAL_WIDTH,'center'
+)
+    push.apply('end')
 end
